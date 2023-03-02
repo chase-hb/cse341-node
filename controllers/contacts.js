71644257a7af5fb44aa81f1e146f8cc17420aff9
thepanda32 <chasebush09@gmail.com>
2023-03-01 23:04:43 -0700

    Added Create, update, and delete funcionality

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -40,5 +40,83 @@ async function getSingle (req, res, next){
 
 }
 
-module.exports = { getAll, getSingle };
+async function createContact (req, res, next){
+
+  console.log("--Creating Contact--")
+
+  const dbQuery = await mongodb.getDb().db().collection('Contacts').insertOne({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  });
+
+  if (dbQuery.acknowledged){
+    console.log("Contact with ID: " + dbQuery.insertedId + " added.")
+    res.status(201).json(dbQuery)
+  } else{
+    console.log("ERROR! Contact creation failed.")
+    res.status(500).json(dbQuery)
+  }
+
+}
+
+
+
+async function updateContact (req, res, next){
+
+  const userId = new ObjectId(req.params.id);
+  console.log("--Updating Contact '" + userId + "' --")
+
+  const dbQuery = await mongodb.getDb().db().collection('Contacts').replaceOne({ _id: userId }, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  });
+
+  if (dbQuery.acknowledged){
+    if(dbQuery.matchedCount > 0){
+      console.log("Found " + dbQuery.matchedCount + " matches.");
+      console.log(dbQuery.modifiedCount + " item(s) modified.")
+      res.status(201).json(dbQuery)
+    } else{
+      console.log("Sorry, no matches found for ID: " + userId);
+      console.log("No modifications made.")
+      res.status(204).send();
+    }
+    
+  } else{
+    console.log("ERROR! Contact update failed.")
+    res.status(500).json(dbQuery)
+  }
+  
+  
+
+}
+
+
+async function deleteContact (req, res, next){
+
+  const userId = new ObjectId(req.params.id);
+  const dbQuery = await mongodb.getDb().db().collection('Contacts').deleteOne({ _id: userId }, true);
+
+  console.log("--Deleting contact '" + userId + "' --");
+
+  if (dbQuery.deletedCount > 0){
+    console.log(dbQuery.deletedCount + " item(s) removed");
+    res.status(204).send();
+  } else if (dbQuery.acknowledged){
+    console.log("ERROR! Delete process did not succeed. Maybe that ID doesn't exist?");
+    res.status(500).json(dbQuery);
+  } else{
+    console.log("ERROR! Delete process did not succeed.");
+    res.status(500).json(dbQuery);
+  }
+
+}
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact};
 
